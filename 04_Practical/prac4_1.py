6f74171ae7948695086b725501945ac42ddd67ca
ThakkarKartik1908 <thakkarkartik198@gmail.com>
2026-09-18 16:37:57 +0530

    Calculate mean median mode variance and standard deviation of student marks

---
+++ 04_Practical/prac4_1.py
@@ -0,0 +1,9 @@
+# parcatical 4.1
+import statistics
+n = [75, 80, 65, 90, 80, 70, 85, 80, 95, 75]
+
+print("mean = " , statistics.mean(n))
+print("median = ", statistics.median(n))
+print("mode = ", statistics.mode(n))
+print("variance = ", statistics.variance(n))
+print("standard deviation = ", statistics.stdev(n))
