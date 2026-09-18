5fa9f2d2af0cdf7eaa9dc455233d9544c1870a57
ThakkarKartik1908 <thakkarkartik198@gmail.com>
2026-09-18 16:37:58 +0530

    Create NumPy arrays and perform basic arithmetic operations

---
+++ 05_Practical/prac5_1.py
@@ -0,0 +1,25 @@
+#Practical 5.1
+import numpy as np
+n = np.array([1,2,3,4,5,6,7,8,9,10])
+m = np.array([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+    ])
+print("1D array")
+print(n)
+
+print("1D Array after Addition:")
+print(n + 5)
+
+print("1D Array after Multiplication:")
+print(n * 2)
+
+print("2D array")
+print(m)
+
+print("2D Array after Addition:")
+print(m + 5)
+
+print("2D Array after Multiplication:")
+print(m * 2)
